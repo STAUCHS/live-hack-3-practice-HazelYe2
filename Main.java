86c01eb192b6b51e19395a209988a25eaadd46c0
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-26 12:47:06 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -5,4 +5,45 @@ class Main extends ConsoleProgram {
 
   // Define your methods and and add javadocs for each one:
 
+  //dateFashion
+  //public int dateFashion(int you, int date) {
+  //  if (you <= 2 || date <= 2) {
+  //    return 0;
+  //  }  
+  //  else if (you >= 8 || date >= 8) {
+  //    return 2;
+  //  }
+  //  else {
+  //    return 1;
+  //  }
+  
+  // teenSum
+  //public int teenSum(int a, int b) {
+  //  if (a >= 13 && a <= 19 || b >= 13 && b <= 19 ) {
+  //    return 19;
+  //  }
+  //    return a + b;
+  //}
+
+  // sortaSum
+  //public int sortaSum(int a, int b) {
+  //  if (a + b >= 10 && a + b <= 19) {
+  //    return 20;
+  //  }
+  //  return a + b;
+
+  // fizzString
+  //public String fizzString(String str) {
+  //  if (str.startsWith ("f") && str.endsWith ("b")) {
+  //    return "FizzBuzz";
+  //  }
+  //  else if (str.startsWith ("f")) {
+  //    return "Fizz";
+  //  }
+  //  else if (str.endsWith ("b")) {
+  //    return "Buzz";
+  //  }
+  //  return str;
+  //}
+  
 }
